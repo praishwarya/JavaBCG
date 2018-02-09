77d1cc2076fdde7b65319697c656adcbc5766c12
praishwarya <pr.aishwarya@gmail.com>
2018-02-09 23:11:13 +0530

    Ignored comments

--- bcg.py
+++ bcg.py
@@ -35,8 +35,6 @@ class JavaLex(object):
     t_NUMBER = r'\.?[0-9][0-9eE_lLdDa-fA-F.xXpP]*'
     t_CHAR_LITERAL = r'\'([^\\\n]|(\\.))*?\''
     t_STRING_LITERAL = r'\"([^\\\n]|(\\.))*?\"'
-    t_LINE_COMMENT = '//.*'
-    t_BLOCK_COMMENT =  r'/\*(.|\n)*?\*/'
     t_OR = r'\|\|'
     t_AND = '&&'
     t_EQUAL = '=='
@@ -61,7 +59,8 @@ class JavaLex(object):
     t_MINUSMINUS = r'\-\-'
     t_ELLIPSIS = r'\.\.\.'
     t_ignore = ' \t\f'
-    
+    t_ignore_COMMENT = r'//.* | /\*(.|\n)*?\*/' 
+ 
     def t_NAME(self, t):
         '[A-Za-z_$][A-Za-z0-9_$]*'
         if t.value in JavaLex.keywords:
@@ -81,39 +80,29 @@ class JavaLex(object):
         t.lexer.skip(1)
 
 if __name__ == '__main__':
-    data_t=['void','byte', 'short', 'int', 'long', 'char', 'float', 'double', 'boolean', 'null']
-    data_t += [kwd.upper() for kwd in data_t]
     JavaLexer = lex.lex(module = JavaLex())
-    my_inp = open('Simple.java','r').read()
+    my_inp = open('Simple1.java','r').read()
     JavaLexer.input(my_inp)
     sym_table=dict()
+    s=dict()
+    sym_table['outer']=s
     flag=0
-    r=random.randint(1,101)
-    top = 0
-    scope = [r]
-    curr_scopes=[r]
+    i = 0
+    l=[]
     for token in JavaLexer:
-        #print(token)
+        print(token)
         if(token.type=='{'):
-           flag=1
-           r=random.randint(1,101)
-           while(r in curr_scopes):
-             r=random.randint(1,101)
-           top = top+1
-           scope.append(r)
+           l.append(s)
+           s=dict()
         if(token.type=='}'):
-           flag=0
-           top = top-1
-           scope.pop()
-        if(flag==1):
-           if(token.type=='NAME'):
-              curr_scopes.append(scope)
-              sym_table[(token.value,scope[top])]=(token.lineno, token.lexpos)
-        else:
-           if(token.type=='NAME'):
-              curr_scopes.append(scope)
-              sym_table[(token.value,scope[top])]=(token.lineno, token.lexpos)
-        prev=token
+           i=i+1
+           sym_table["inner_"+str(i)]=s
+           print("\n\n[Table for scope: inner_"+str(i),"]\n")
+           print(tabulate([[sym, s[sym]] for sym in s], headers = ['NAME', 'VALUE'], tablefmt='orgtbl'))
+           s=l.pop()
+        if(token.type=='NAME'):
+              s[token.value]=(token.lineno, token.lexpos)
+    sym_table['outer']=s
+    print("\n\n[Table for scope: outer]\n\n")
+    print(tabulate([[sym, s[sym]] for sym in s], headers = ['NAME', 'VALUE'], tablefmt='orgtbl'))
     #print(sym_table)
-    print(tabulate([[sym,s,sym_table[(sym,s)]] for (sym,s) in sym_table], headers = ['NAME','SCOPE','VALUE'], tablefmt='orgtbl'))
-    #print(tabulate(list(sym_table), headers = ['Identifier', 'Line.no','Column.no','Scope'], tablefmt='orgtbl'))
